03ef1744cdbd356c7437a853e781f875588dc064
Nicolasopf <69660332+Nicolasopf@users.noreply.github.com>
2021-02-11 20:46:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,35 +5,34 @@ import urllib
 from os import path, makedirs, environ
 
 
-Home=environ['HOME'] + '/.hbtn_utils/cookie.txt'
-if path.exists(Home) is False:
+Home = environ['HOME'] + '/.hbtn_utils/cookie.txt'
+pat = Home[:-11]
+if path.exists(pat) is False:
     print("Please enter your username: ", end='')
     user = input()
     password = getpass()
-    makedirs(Home, exist_ok=True)
-    """ WE NEED TO DO THE REQUEST TO LOGIN """
-    # if session.get == 200:
-    #     with open("/home/ubuntu/.hbtn_utils/cookie.txt", "w") as user_cookie:
-    #         user_cookie.write("Here should be the cookie")
-    # else:
+    with requests.Session() as log:
+        payload={
+            "user[login]": user,
+            "user[password]": password,
+            "commit": "Log in"
+        }
+        r = log.get("https://intranet.hbtn.io/auth/sign_in")
+        soup = BeautifulSoup(r.content, 'html5lib')
+        payload['authenticity_token'] = soup.find('input', attrs={'name': 'authenticity_token'})['value']
+        pos = log.post("https://intranet.hbtn.io/auth/sign_in", data=payload)
 
+        if "Invalid Email or password." in pos.text:
+            print("Your credentials are wrong. Please try again.")
+            exit() # Or do a loop.
+        hbtn_cookie=pos.cookies.get_dict()
+        makedirs(pat, exist_ok=True)
+        with open(Home, "w") as file:
+            file.write(str(hbtn_cookie))
 
-with requests.Session() as log:
-    payload={
-        "user[login]": user,
-        "user[password]": password,
-        "commit": "Log in"
-    }
-    r = log.get("https://intranet.hbtn.io/auth/sign_in")
-    soup = BeautifulSoup(r.content, 'html5lib')
-    payload['authenticity_token'] = soup.find('input', attrs={'name': 'authenticity_token'})['value']
-    print(user)
-    pos = log.post("https://intranet.hbtn.io/auth/sign_in", data=payload)
-    hbtn_cookie=pos.cookies.get_dict()
-    print(hbtn_cookie)
-
-#with open(Home, 'r') as user_cookie:
-#    print(user_cookie.read())
+with open(Home, 'r') as user_cookie:
+    cookie=user_cookie.read()
+print(cookie)
 exit()
 
 """
@@ -54,7 +53,7 @@ content = tmp.content
 web = BeautifulSoup(content, 'html.parser')
 
 # Search for data-confirm
-hml = web.find("a", {"data-confirm": True})
+hml = web.find_all("a", {"data-confirm": True})
 if hml:
     opt = sexsion.get(url + '/unlock_optionals', cookies=cookie)
 print(hml)
