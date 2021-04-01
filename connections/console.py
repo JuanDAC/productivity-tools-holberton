2fdd4d6b92c3a78c53f3f5251aae5d8f8901b561
Nicolas <n.ico15935746@gmail.com>
2021-04-01 03:02:12 -0500

    [Added] Basic console handling EOF, quit. connection successful with the cookie. (Next to do: show the actual projects)

---
+++ connections/console.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+import cmd
+from logs.environment import get_cookie
+from bs4 import BeautifulSoup
+import requests
+""" Interactive console to get projects, review, files, and more. """
+
+if __name__ == "__main__":
+    print("Use main.py instead of console.py")
+    exit()
+
+
+class Console(cmd.Cmd):
+    """
+    Interactive console
+    prompt = Console prompt waiting for input
+
+    =====================================================
+    |     In case to add or edit a help description     |
+    |  Modify the comment below function of instruction |
+    =====================================================
+
+    """
+    prompt = "Console-> "
+    cookie = get_cookie()
+
+    def __init__(self):
+        """ Stablish the connection to persistent """
+        cmd.Cmd.__init__(self)
+        self.connection = requests.Session()
+        """
+        The next request is because self.connection save the session, so
+        we don't need to specify the cookie again in all this file.
+        Why this specific website? Is the faster page to load.
+        """
+        self.connection.get("https://intranet.hbtn.io/dashboards/my_curriculums",
+                            cookies=self.cookie)
+
+    def emptyline(self):
+        """ User enters an empty line >> pass """
+        pass
+
+    def do_EOF(self, line):
+        """ User send EOF (End of file | CTRL + D) >> exit """
+        print()
+        return True
+
+    def do_quit(self, line):
+        """ User types quit >> exit """
+        return True
+
+    def do_get_projects(self, line):
+        """ Show projects running at the moment"""
+        html = self.connection.get(
+            "https://intranet.hbtn.io/dashboards/my_current_projects")
+        print(html.text)
+
+
+console_loop = Console()
+console_loop.cmdloop()
+del (console_loop.connection)
