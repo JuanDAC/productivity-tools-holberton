454a2203976f3506e41f8441bc35cca0642ac574
Nicolas <n.ico15935746@gmail.com>
2021-04-01 03:06:39 -0500

    [Updated] Needless print changed text to the input field

--- logs/credentials.py
+++ logs/credentials.py
@@ -3,24 +3,23 @@ from getpass import getpass
 import requests
 import urllib
 
-# TODO add cast and iterative input when will don't done
+
 def login():
-	print("Please enter your username: ", end='')
-	user = input()
-	password = getpass()
-	with requests.Session() as log:
-		payload={
-			"user[login]": user,
-			"user[password]": password,
-			"commit": "Log in"
-		}
-		r = log.get("https://intranet.hbtn.io/auth/sign_in")
-		soup = BeautifulSoup(r.content, 'html5lib')
-		payload['authenticity_token'] = soup.find('input', attrs={'name': 'authenticity_token'})['value']
-		pos = log.post("https://intranet.hbtn.io/auth/sign_in", data=payload)
+    user = input("Please enter your username: ")
+    password = getpass()
+    with requests.Session() as log:
+        payload = {
+            "user[login]": user,
+            "user[password]": password,
+            "commit": "Log in"
+        }
+        r = log.get("https://intranet.hbtn.io/auth/sign_in")
+        soup = BeautifulSoup(r.content, 'html5lib')
+        payload['authenticity_token'] = soup.find(
+            'input', attrs={'name': 'authenticity_token'})['value']
+        pos = log.post("https://intranet.hbtn.io/auth/sign_in", data=payload)
 
-		if "Invalid Email or password." in pos.text:
-			print("Your credentials are wrong. Please try again.")
-			login()
-		return(pos.cookies.get_dict())
-
+        if "Invalid Email or password." in pos.text:
+            print("Your credentials are wrong. Please try again.")
+            login()
+        return(pos.cookies.get_dict())
