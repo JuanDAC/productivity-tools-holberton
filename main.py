ad3eb138737b2d03b33ba9d75debf3920a45b62f
Nicolas <n.ico15935746@gmail.com>
2021-04-01 03:02:52 -0500

    [Added] If cookie already exists, go to the console

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from os import path, makedirs, environ
 from logs.credentials import login
 from logs.environment import *
+from connections.console import Console
+
 
 def main():
-	if exist_directory():
-		create_directory()
-	if not exist_cookie():
-		create_cookie(login())
+    if exist_directory():
+        create_directory()
+    if exist_cookie():
+        Console()
+    else:
+        create_cookie(login())
+
 
 if __name__ == '__main__':
-	main()
+    main()
