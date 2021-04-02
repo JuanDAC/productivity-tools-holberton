67104a32763df36336354f1b92ef3f0c32238077
Nicolas <n.ico15935746@gmail.com>
2021-04-02 14:01:07 -0500

    [Updated] Moved the request to get the projects to the init

--- connections/console.py
+++ connections/console.py
@@ -32,10 +32,20 @@ class Console(cmd.Cmd):
         """
         The next request is because self.connection save the session, so
         we don't need to specify the cookie again in all this file.
-        Why this specific website? Is the faster page to load.
+        Why this specific website? Because is the most important.
+        This gets the current projects and save them in project_list-
+        so when user use get_projects it will be displayed.
         """
-        self.connection.get("https://intranet.hbtn.io/dashboards/my_curriculums",
-                            cookies=self.cookie)
+        print("This could take a bit... (Some seconds)")
+        html = self.connection.get(
+            "https://intranet.hbtn.io/dashboards/my_current_projects",
+            cookies=self.cookie)
+        projects_block_html = re.findall(
+            r"<ul class=\"list-group gap\">.*<span class=\"bpi-status\">",
+            html.text, flags=re.DOTALL)
+        self.project_list = re.findall(r"/[0-9]{1,3}\".*<",
+                                  "\n".join(projects_block_html))
+
 
     def emptyline(self):
         """ User enters an empty line >> pass """
@@ -51,16 +61,9 @@ class Console(cmd.Cmd):
         return True
 
     def do_get_projects(self, line):
-        """ Show projects running at the moment"""
-        html = self.connection.get(
-            "https://intranet.hbtn.io/dashboards/my_current_projects")
-        projects_block_html = re.findall(
-            r"<ul class=\"list-group gap\">.*<span class=\"bpi-status\">",
-            html.text, flags=re.DOTALL)
-        project_list = re.findall(r"/[0-9]{1,3}\".*<",
-                                  "\n".join(projects_block_html))
-        print("\n".join(project_list).
-              replace("\"", " ").
+        """ Show projects running at the moment """
+        print("\n".join(self.project_list).
+              replace("\"", " | ").
               replace("<", "").
               replace(">", "").
               replace("/", ""))
