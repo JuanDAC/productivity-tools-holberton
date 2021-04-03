e3c9aab4e125195ff8609d26bf28ac8057ef8405
Nicolas <n.ico15935746@gmail.com>
2021-04-03 14:37:53 -0500

    [Updated] Fix double init ocurring

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from os import path, makedirs, environ
 from logs.credentials import login
 from logs.environment import *
-from connections.console import Console
 
 
 def main():
     if exist_directory():
         create_directory()
     if exist_cookie():
-        Console()
+        from connections.console import Console
     else:
         create_cookie(login())
 
