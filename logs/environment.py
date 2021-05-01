bcc6d659227fc2f455a81ba8b1405e742fdb2958
Nicolas <n.ico15935746@gmail.com>
2021-05-01 14:25:36 -0500

    [Added] Shebang

--- logs/environment.py
+++ logs/environment.py
@@ -1,26 +1,32 @@
+#!/usr/bin/python3
 from os import path, makedirs, environ
-from logs.env import WORKING_DIRECTORY, COOKIE;
+from logs.env import WORKING_DIRECTORY, COOKIE
 from ast import literal_eval
 
+
 def exist_directory():
-	return(path.exists(WORKING_DIRECTORY))
+    return(path.exists(WORKING_DIRECTORY))
+
 
 def create_directory():
-	try:
-		makedirs(WORKING_DIRECTORY, exist_ok=True)
-	except OSError:
-		print ("Creation of the directory %s failed" % WORKING_DIRECTORY)
-		return(False);
-	else:
-		return(True)
+    try:
+        makedirs(WORKING_DIRECTORY, exist_ok=True)
+    except OSError:
+        print ("Creation of the directory %s failed" % WORKING_DIRECTORY)
+        return(False)
+    else:
+        return(True)
+
 
 def exist_cookie():
-	return(path.isfile(COOKIE))
+    return(path.isfile(COOKIE))
+
 
 def create_cookie(hbtn_cookie):
-	with open(COOKIE, "w") as file:
-		file.write(str(hbtn_cookie))
-def get_cookie():
-	with open(COOKIE, 'r') as user_cookie:
-		return(literal_eval(ser_cookie.read()))
+    with open(COOKIE, "w") as file:
+        file.write(str(hbtn_cookie))
 
+
+def get_cookie():
+    with open(COOKIE, 'r') as user_cookie:
+        return(literal_eval(user_cookie.read()))
